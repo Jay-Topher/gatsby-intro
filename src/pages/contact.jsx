c508ac628c77bd1bb317b3f3640466be30d2ee9a
Jones Ogolo <jay.topher11@gmail.com>
2020-07-28 23:44:00 +0100

    created contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact Me</h1>
+      <p>Feel free to reach me on</p>
+      <ul>
+        <li>Jay.topher11@gmail.com</li>
+        <li>jones.ogolo@gmail.com</li>
+        <li>
+          <a href="https://www.twitter.com/mr_jonce" target="_blank" rel="noreferrer">
+            Follow Me
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  )
+}
+
+export default ContactPage
