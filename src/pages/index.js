e5080f3073d67d71b26cb7a192db65f4a877db22
Jones Ogolo <jay.topher11@gmail.com>
2020-07-28 23:43:27 +0100

    created home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import {Link} from 'gatsby'
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Hi! I'm Jones</h1>
+      <h2>A fullstack developer living in Lagos.</h2>
+      <p>Need a developer? <a href="/contact">Contact Me.</a></p>
+      <p>Need a developer? <Link to="/contact">Contact Me.</Link></p>
+    </Layout>
+  )
 }
