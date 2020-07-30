56597f6a208007ff5c7e22ce1e05bbc008edd0bf
Jones Ogolo <jay.topher11@gmail.com>
2020-07-30 01:16:26 +0100

    updated blog to show list of markdown posts

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,11 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(post => (
+          <li>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p>
+            <Link to={`/blog/${post.node.fields.slug}`}>See post</Link>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
